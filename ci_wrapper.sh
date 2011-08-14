aa39aafd498af75835c6a23e10aaa940a4a3d607
Brian Lalor <blalor@bravo5.org>
2011-08-14 10:53:51 -0400

    ci wrapper tweaks

--- ci_wrapper.sh
+++ ci_wrapper.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-make -C test/support/MockAVR all
-make -C test test
-rc=$?
+make -C test/support/MockAVR all; rc1=$?
+make -C test test; rc2=$?
+make -C main all; rc3=$?
 
-if [ $rc -ne 0 ]; then
+rc=0
+if [ $rc1 -ne 0 ] || [ $rc2 -ne 0 ] || [ $rc3 -ne 0 ]; then
     growlnotify -a Xcode -n simple-ci -d bravo5.simpleci -t CI -m "BUILD FAILED" 
+    rc=1
 # else
 #     growlnotify -a Xcode -n simple-ci -d bravo5.simpleci -m "Success" 
 fi
